87749f2d70872a59a74c0b653f552fdc15ab121a
Eternal_1ove <shadow-ek-alex@mail.ru>
2024-10-26 03:56:16 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        sas
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "App",
+    mounted() {
+        alert("dorou")
+    }
+}
+</script>
+
+<style scoped lang="scss">
+div {
+  background: #876CA8;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './assets/style/reset.css'
+import './assets/style/main.scss'
+
+createApp(App).mount('#app')
